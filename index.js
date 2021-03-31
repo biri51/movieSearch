6a6325f968c90a86246f1e03a1325b45c880bd70
biri51 <45977047+biri51@users.noreply.github.com>
2021-03-31 18:21:26 -0300

    Add files via upload
    atualizado

--- index.js
+++ index.js
@@ -6,25 +6,53 @@ const fetchData = async (searchTerm) => {
         s: searchTerm
     }
 });
+
+if (response.data.Error){ //não aparecer erro quando procurar algo que não tem no API
+    return [];
+}
+
 return response.data.Search;
 }
 
-const input = document.querySelector('input')
+const root = document.querySelector('.autocomplete') //CSS para o searchbar. Documentação Bulma.
+root.innerHTML =`
+<label><b>Search for a Movie</b></label>
+<input class="input"/>
+<div class="dropdown"> 
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>     
+    </div>
+</div>`
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');//seleciona o div com class dropdown, para tornar ativo na search
+const resultsWrapper = document.querySelector('.results');//seleciona o dropdown item com class results
 
 
 const onInput = async (ev) => {   //pq fetchdata é um async function, temos que colocar async aqui tb para termos resultados
-   const movies =  await fetchData(ev.target.value); // se não colocarmos await, retornará uma promise
-   
- 
-    for (let movie of movies) {
-        const div = await document.createElement('div');
-
-    div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
-    `; 
-    document.querySelector('#target').appendChild(div);
-    }
-   };
+    const movies =  await fetchData(ev.target.value); // se não colocarmos await, retornará uma promise
     
-input.addEventListener('input', debounce(onInput));
+    resultsWrapper.innerHTML =''; //para limpar a busca feita, caso tenha tido uma anterior
+    dropdown.classList.add('is-active')//fazendo o menu ficar ativo quando digitar algo
+  
+     for (let movie of movies) {
+        const option = document.createElement('a');//na documentação pede que seja uma anchor tag
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;  // ternary expression para caso houver nenhuma imagem de poster 
+
+        option.classList.add('dropdown-item') //pra aparecer os itens no search
+        option.innerHTML = `
+         <img src="${imgSrc}" />
+         ${movie.Title}
+     `; 
+    
+     resultsWrapper.appendChild(option);
+     }
+    };
+     
+ input.addEventListener('input', debounce(onInput));
+
+ document.addEventListener('click', event => { //evento para clicar fora do search bar e widget para sumir a busca
+     if (!root.contains(event.target));{ //se não estiver no root, tirar o is active
+     dropdown.classList.remove('is-active');
+    }
+ });
